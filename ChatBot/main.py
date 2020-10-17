d3698f7100fdab5439e42374ebb16b108dd14bfc
aditya mangal <adityamangal0202@gmail.com>
2020-10-17 14:49:09 +0530

    initial commit

--- ChatBot/main.py
+++ ChatBot/main.py
@@ -1,6 +1,6 @@
 '''
 Made by - Aditya mangal
-Purpose - Task submission
+Purpose - Python mini project
 Date  - 18 october 2020
 '''
 from chatterbot import ChatBot
@@ -13,21 +13,6 @@ trainer = ChatterBotCorpusTrainer(chatbot)
 trainer.train('chatterbot.corpus.english')
 
 
-def Enrollment():
-    print('Fill all the following details to submit the form.\n')
-    user_name = input('Enter your name: ')
-    user_location = input(
-        'Enter your location which was suitable for the exam: \n')
-    user_field_of_work = input('Enter your field of work: ')
-    submission_date = input('Enter submission date: ')
-    print('Searching nearest location.......')
-    time.sleep(3)
-    print('According to your submiited date', submission_date, 'and location',
-          user_location, '\nLocations assigned to you are: Noida')
-    print('Locations assigned to you are: Delhi ')
-    print('Good luck!!')
-
-
 print('\t\t\t A Chatot')
 print('Type enrollment forms to enroll otherwise continue to talk with ChatBot')
 print('You can exit by type exit\n')
@@ -35,7 +20,5 @@ while True:
     query = input(">> ")
     if 'exit' in query:
         exit()
-    elif 'Enrollment Forms' and 'enrollment forms' in query:
-        Enrollment()
     else:
         print(chatbot.get_response(query))
